86483e7539693e7693d72b06a999d273929cf2d5
Mini Model <minimodel@metamodel.me>
2019-01-03 20:20:29 +0100

    Update PHPDoc file headers

--- contao/config/event_listeners.php
+++ contao/config/event_listeners.php
@@ -3,19 +3,18 @@
 /**
  * This file is part of MetaModels/attribute_tabletext.
  *
- * (c) 2012-2016 The MetaModels team.
+ * (c) 2012-2019 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
  * This project is provided in good faith and hope to be usable by anyone.
  *
- * @package    MetaModels
- * @subpackage AttributeTableText
+ * @package    MetaModels/attribute_tabletext
  * @author     Christopher Boelter <christopher@boelter.eu>
  * @author     Ingolf Steinhardt <info@e-spin.de>
- * @copyright  2012-2016 The MetaModels team.
- * @license    https://github.com/MetaModels/attribute_tabletext/blob/master/LICENSE LGPL-3.0
+ * @copyright  2012-2019 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_tabletext/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
