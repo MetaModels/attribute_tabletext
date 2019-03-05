5fae3def525228ab0a7d2249fbd0cedf644664e3
Mini Model <minimodel@metamodel.me>
2019-03-05 19:37:45 +0100

    Update PHPDoc class headers

--- tests/Test/DeprecatedAutoloaderTest.php
+++ tests/Test/DeprecatedAutoloaderTest.php
@@ -25,8 +25,6 @@ use PHPUnit\Framework\TestCase;
 
 /**
  * This class tests if the deprecated autoloader works.
- *
- * @package MetaModels\AttributeTableTextBundle\Test
  */
 class DeprecatedAutoloaderTest extends TestCase
 {
