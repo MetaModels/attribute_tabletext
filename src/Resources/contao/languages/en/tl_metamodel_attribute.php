6a1e0a150fdc75692d8397caa69eb348260bcb38
zonky2 <info@e-spin.de>
2020-11-10 18:58:10 +0100

    Add count for min and max columns

--- src/Resources/contao/languages/en/tl_metamodel_attribute.php
+++ src/Resources/contao/languages/en/tl_metamodel_attribute.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_tabletext.
  *
- * (c) 2012-2016 The MetaModels team.
+ * (c) 2012-2020 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -13,7 +13,8 @@
  * @package    MetaModels
  * @subpackage AttributeTableText
  * @author     David Maack <david.maack@arcor.de>
- * @copyright  2012-2016 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2020 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_tabletext/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
@@ -21,7 +22,15 @@
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tabletext']    = 'Text table';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_cols']                = array('Column config', '');
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowLabel']            = array('Label', 'Please enter ther labels for the columns');
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowStyle']            = array('Width', 'Please enter the width of the columns (e.g. 200px)');
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tabletext']  = 'Text table';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_cols']            = ['Column config', ''];
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowLabel']        =
+    ['Label', 'Please enter ther labels for the columns'];
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowStyle']        =
+    ['Width', 'Please enter the width of the columns (e.g. 200px)'];
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_minCount']        =
+    ['Minimum count of rows', 'Please enter minimum count of rows - 0 sets the value without limit'];
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_maxCount']        =
+    ['Maximum count of rows', 'Please enter maximum count of rows - 0 sets the value without limit'];
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_disable_sorting'] =
+    ['Disable sorting', 'If this option is activated, the sorting is hidden.'];
