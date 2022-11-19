08a6140af8de88e4937c83bfc857c63dcebd0a6f
Mini Model <minimodel@metamodel.me>
2022-11-19 18:00:08 +0100

    Updated translations from transifex.

--- src/Resources/contao/languages/de/tl_metamodel_attribute.php
+++ src/Resources/contao/languages/de/tl_metamodel_attribute.php
@@ -1,19 +1,36 @@
 <?php
 /**
- * Translations are managed using Transifex. To create a new translation
+ * This file is part of MetaModels/attribute_tabletext.
+ *
+ * (c) 2012-2022 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * Translations are managed automatically using Transifex. To create a new translation
  * or to help to maintain an existing one, please register at transifex.com.
  *
- * @link http://help.transifex.com/intro/translating.html
- * @link https://www.transifex.com/projects/p/metamodels/language/de/
+ * Last-updated: 2022-11-19T17:59:55+01:00
  *
- * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
- *
- * last-updated: 2013-06-24T16:17:03+02:00
+ * @copyright 2012-2022 The MetaModels team.
+ * @license   https://github.com/MetaModels/attribute_tabletext/blob/master/LICENSE LGPL-3.0-or-later
+ * @link      https://www.transifex.com/metamodels/public/
+ * @link      https://www.transifex.com/signup/?join_project=metamodels
  */
 
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_cols']['0']      = 'Spalteneinstellung';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowLabel']['0']  = 'Label';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowLabel']['1']  = 'Bitte geben Sie die Labels für die Spalten an.';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowStyle']['0']  = 'Breite';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowStyle']['1']  = 'Bitte geben Sie die Spaltenbreite ein (beispielsweise 200px).';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tabletext'] = 'Text-Tabelle';
+
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_cols']['0']            = 'Spalteneinstellung';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_disable_sorting']['0'] = 'Sortierung deaktivieren';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_disable_sorting']['1'] = 'Wenn diese Option aktiviert ist, wird die Sortierung ausgeblendet.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_maxCount']['0']        = 'Maximale Anzahl von Zeilen';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_maxCount']['1']        = 'Bitte geben Sie die maximale Anzahl der Zeilen ein - 0 setzt den Wert ohne Begrenzung.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_minCount']['0']        = 'Mindestanzahl der Zeilen';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_minCount']['1']        = 'Bitte geben Sie die Mindestanzahl der Zeilen ein - 0 setzt den Wert ohne Begrenzung.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowLabel']['0']        = 'Label';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowLabel']['1']        = 'Bitte geben Sie die Labels für die Spalten an.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowStyle']['0']        = 'Breite';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['tabletext_rowStyle']['1']        = 'Bitte geben Sie die Spaltenbreite ein (beispielsweise 200px).';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['tabletext']       = 'Text-Tabelle';
+
