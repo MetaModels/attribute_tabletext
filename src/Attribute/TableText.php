5e44bcd2d0b69c3217d1e0df6d8a131f83e6608b
Ingolf Steinardt <info@e-spin.de>
2024-04-24 18:28:14 +0200

    Fixes for PHPCQ2

--- src/Attribute/TableText.php
+++ src/Attribute/TableText.php
@@ -34,6 +34,18 @@ use Doctrine\DBAL\Connection;
 use MetaModels\Attribute\BaseComplex;
 use MetaModels\IMetaModel;
 
+use function array_column;
+use function array_keys;
+use function array_map;
+use function array_merge;
+use function array_search;
+use function count;
+use function implode;
+use function is_array;
+use function is_int;
+use function str_replace;
+use function time;
+
 /**
  * This is the MetaModelAttribute class for handling table text fields.
  *
@@ -81,9 +93,10 @@ class TableText extends BaseComplex
      */
     public function searchFor($strPattern)
     {
-        $strPattern = \str_replace(['*', '?'], ['%', '_'], $strPattern);
+        $strPattern = str_replace(['*', '?'], ['%', '_'], $strPattern);
 
-        return $this->connection->createQueryBuilder()
+        $statement = $this->connection
+            ->createQueryBuilder()
             ->select('t.item_id')
             ->from($this->getValueTable(), 't')
             ->where('t.value LIKE :pattern')
@@ -92,6 +105,9 @@ class TableText extends BaseComplex
             ->setParameter('id', $this->get('id'))
             ->executeQuery()
             ->fetchFirstColumn();
+
+        // Return value list as list<mixed>, parent function wants a list<string> so we make a cast.
+        return array_map(static fn (mixed $value) => (string) $value, $statement->fetchFirstColumn());
     }
 
     /**
@@ -99,7 +115,7 @@ class TableText extends BaseComplex
      */
     public function getAttributeSettingNames()
     {
-        return \array_merge(
+        return array_merge(
             parent::getAttributeSettingNames(),
             ['tabletext_cols', 'tabletext_minCount', 'tabletext_maxCount', 'tabletext_disable_sorting']
         );
@@ -140,7 +156,7 @@ class TableText extends BaseComplex
 
         $arrFieldDef['eval']['columnFields'] = [];
 
-        $countCol = \count($arrColLabels);
+        $countCol = count($arrColLabels);
         for ($i = 0; $i < $countCol; $i++) {
             // Init columnField.
             $arrFieldDef['eval']['columnFields']['col_' . $i] = [
@@ -175,7 +191,7 @@ class TableText extends BaseComplex
         }
 
         // Get the ids.
-        $arrIds = \array_keys($arrValues);
+        $arrIds = array_keys($arrValues);
 
         // Reset all data for the ids.
         $this->unsetDataFor($arrIds);
@@ -224,7 +240,7 @@ class TableText extends BaseComplex
         while ($objRow = $statement->fetchAssociative()) {
             $strValue = $objRow['value'];
 
-            if (\is_array($arrCount)) {
+            if (is_array($arrCount)) {
                 $arrCount[$strValue] = $objRow['mm_count'];
             }
 
@@ -307,14 +323,14 @@ class TableText extends BaseComplex
         $strWhereIds = '';
         $strRowCol   = '';
         if ($mixIds) {
-            if (\is_array($mixIds)) {
-                $strWhereIds = ' AND ' . $tableAlias . 'item_id IN (' . \implode(',', $mixIds) . ')';
+            if (is_array($mixIds)) {
+                $strWhereIds = ' AND ' . $tableAlias . 'item_id IN (' . implode(',', $mixIds) . ')';
             } else {
                 $strWhereIds = ' AND ' . $tableAlias . 'item_id=' . $mixIds;
             }
         }
 
-        if (\is_int($intRow) && \is_int($intCol)) {
+        if (is_int($intRow) && is_int($intCol)) {
             $strRowCol = ' AND ' . $tableAlias . 'row = :row AND ' . $tableAlias . 'col = :col';
         }
 
@@ -335,17 +351,17 @@ class TableText extends BaseComplex
      */
     public function valueToWidget($varValue)
     {
-        if (!\is_array($varValue)) {
+        if (!is_array($varValue)) {
             return [];
         }
 
         $arrColLabels = StringUtil::deserialize($this->get('tabletext_cols'), true);
-        $countCol     = \count($arrColLabels);
+        $countCol     = count($arrColLabels);
         $widgetValue  = [];
 
         foreach ($varValue as $k => $row) {
             for ($kk = 0; $kk < $countCol; $kk++) {
-                $index = \array_search($kk, \array_column($row, 'col'), true);
+                $index = array_search($kk, array_column($row, 'col'), true);
 
                 $widgetValue[$k]['col_' . $kk] = ($index !== false) ? $row[$index]['value'] : '';
             }
@@ -360,7 +376,7 @@ class TableText extends BaseComplex
     public function widgetToValue($varValue, $itemId)
     {
         /** @var array<int, array<string, string>>|null|string $varValue */
-        if (!\is_array($varValue)) {
+        if (!is_array($varValue)) {
             return [];
         }
 
@@ -373,7 +389,7 @@ class TableText extends BaseComplex
             foreach ($row as $colName => $value) {
                 $newRow[] = [
                     'value' => $value,
-                    'col' => (int) \str_replace('col_', '', $colName),
+                    'col' => (int) str_replace('col_', '', $colName),
                     'row' => $intRow,
                 ];
             }
@@ -395,7 +411,7 @@ class TableText extends BaseComplex
     protected function getSetValues($arrCell, $intId)
     {
         return [
-            $this->getValueTable() . '.tstamp'  => \time(),
+            $this->getValueTable() . '.tstamp'  => time(),
             $this->getValueTable() . '.value'   => (string) $arrCell['value'],
             $this->getValueTable() . '.att_id'  => $this->get('id'),
             $this->getValueTable() . '.row'     => (int) $arrCell['row'],
@@ -416,7 +432,7 @@ class TableText extends BaseComplex
     private function pushValue(array $value, array &$result, int $countCol): void
     {
         $buildRow = function (array &$list, int $itemId, int $row) use ($countCol): void {
-            for ($i = \count($list); $i < $countCol; $i++) {
+            for ($i = count($list); $i < $countCol; $i++) {
                 $list[$i] = [
                     'tstamp'  => 0,
                     'value'   => '',
@@ -434,7 +450,7 @@ class TableText extends BaseComplex
         }
 
         // Prepare all rows up until to this item.
-        $row = \count($result[$itemId]);
+        $row = count($result[$itemId]);
         while ($row <= $value['row']) {
             if (!isset($result[$itemId][$row])) {
                 $result[$itemId][$row] = [];
