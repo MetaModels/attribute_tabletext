9c09f8d6299312f8dc4c58199bd3a8d25a54cc3f
Ingolf Steinardt <info@e-spin.de>
2024-07-19 22:25:36 +0200

    Changes for translation loader

--- src/Resources/contao/dca/tl_metamodel_rendersetting.php
+++ src/Resources/contao/dca/tl_metamodel_rendersetting.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_tabletext.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2024 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -18,25 +18,23 @@
  * @author     Ingolf Steinhardt <info@e-spin.de>
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2012-2019 The MetaModels team.
+ * @copyright  2012-2024 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_tabletext/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
 
-$GLOBALS['TL_DCA']['tl_metamodel_rendersetting']['metapalettes']['tabletext extends default'] = array
-(
-    '+advanced' => array('tabletext_hide_tablehead'),
-);
+$GLOBALS['TL_DCA']['tl_metamodel_rendersetting']['metapalettes']['tabletext extends default'] = [
+    '+advanced' => ['tabletext_hide_tablehead'],
+];
 
 
-$GLOBALS['TL_DCA']['tl_metamodel_rendersetting']['fields']['tabletext_hide_tablehead'] = array
-(
-    'label'     => &$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['tabletext_hide_tablehead'],
-    'exclude'   => true,
-    'inputType' => 'checkbox',
-    'eval'      => array
-    (
+$GLOBALS['TL_DCA']['tl_metamodel_rendersetting']['fields']['tabletext_hide_tablehead'] = [
+    'label'       => 'tabletext_hide_tablehead.label',
+    'description' => 'tabletext_hide_tablehead.description',
+    'exclude'     => true,
+    'inputType'   => 'checkbox',
+    'eval'        => [
         'tl_class' => 'clr w50'
-    ),
-    'sql'       => 'varchar(1) NOT NULL default \'0\''
-);
+    ],
+    'sql'         => 'varchar(1) NOT NULL default \'0\''
+];
